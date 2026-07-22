5ade482177281b42de369afa8487f67f531cfce1
Mazharul Islam <mazharulrafi@gmail.com>
2026-07-22 15:52:18 +0600

    Update project files

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,5 @@
+import FiverShieldTool from '@/components/FiverShieldTool';
+
 export default function Home() {
-  return (
-    <main className="min-h-screen min-w-screen overflow-hidden bg-black flex items-center justify-center">
-      <img
-        src="/icon_images/siu.jpg"
-        alt="Siu"
-        className="w-full h-full object-contain"
-      />
-    </main>
-  );
+  return <FiverShieldTool />;
 }
